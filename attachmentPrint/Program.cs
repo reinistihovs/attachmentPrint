39e78a606ffad62ebb609fccdae161121d1ca9c5
r.tihovs <r.tihovs@gmail.com>
2021-10-27 00:23:59 +0300

    added internet check, added tests

--- attachmentPrint/Program.cs
+++ attachmentPrint/Program.cs
@@ -13,22 +13,36 @@ namespace attachmentPrint
         {
             var appConfiguration = new appConfiguration();
             var getAttachments2 = new getAttachments2();
+            Console.WriteLine("Checking internet connection");
+            bool isConnected = getAttachments2.checkConnection("imap.gmail.com");
+            if (!isConnected) {
+                Console.WriteLine("cant connect to gmail, do you have internet?");
+                Console.WriteLine("Press Enter N to Exit... ");
+                while (Console.ReadKey().Key != ConsoleKey.N) {}
+                return;
+
+            }
             Console.WriteLine("Please choose action:");
-            Console.WriteLine("1) Print all attachments in INBOX");
-            Console.WriteLine("2) Print Unread attachments in INBOX and mark as read");
-            Console.WriteLine("3) Print unregistered attachment and register email ID in database");
+            Console.WriteLine("1)  to download all attachments from INBOX and move messages to Processed folder");
+            //Console.WriteLine("2) Print Unread attachments in INBOX and mark as read");
+            //Console.WriteLine("3) Print unregistered attachment and register email ID in database");
             Console.WriteLine("press any other key to EXIT");
             //int Choice = 0;
             int Action = int.Parse(Console.ReadLine());
-            //if (Action != 1 || Action != 2 || Action != 3)
-            //{
-            //    return;
+            if (Action == 1 )
+            {
+                getAttachments2.downloadAllAttachments();
 
-            //}
+            }
+            else
+            {
+                Console.WriteLine($"Invalid action selected press ENTER to exit");
+                while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+            }
             
             //if (Action == 1)
            // {
-                getAttachments2.downloadAllAttachments();
+                
 
            // }
 
